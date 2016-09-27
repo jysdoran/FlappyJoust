f8c60bf3f47625057ef93c8e36baad571e08e315
James Doran <jysdoran@gmail.com>
2016-09-27 03:19:37 +0100

    Menu Screen

---
+++ Assets/Scripts/SceneControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class SceneControl : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //Scene scene = SceneManager.GetActiveScene();
+        DontDestroyOnLoad(this);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Scenes/Main");
+    }
+
+/*    public void StartCollection()
+    {
+        SceneManager.LoadScene("Scenes/Collection");
+    }
+*/
+    public void StartMenu()
+    {
+        SceneManager.LoadScene("Scenes/Menu");
+    }
+}
